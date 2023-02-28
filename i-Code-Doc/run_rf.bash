b6ce8e12b2aba927efc16ee081f62b016cf1ebbe
DavidCorreas <davidestu77@gmail.com>
2023-02-28 11:54:01 +0000

    Refactor: Some

--- i-Code-Doc/run_rf.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-PYTHONPATH="." CUDA_VISIBLE_DEVICES=1 python run_robotframework.py run_robotframework_train.json
---
+++ i-Code-Doc/scripts/finetune_robotframework.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+PYTHONPATH="." CUDA_VISIBLE_DEVICES=1 python run_robotframework.py scripts/run_robotframework_train.json
